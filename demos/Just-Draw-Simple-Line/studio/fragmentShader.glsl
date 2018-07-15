77a8ccea1634eea4d972f9f66d2d8fb5a07623e0
jasonChen1982 <jasonchen1882@gmail.com>
2018-07-15 20:43:34 +0800

    feat(light): add some color for mix

--- demos/Just-Draw-Simple-Line/studio/fragmentShader.glsl
+++ demos/Just-Draw-Simple-Line/studio/fragmentShader.glsl
@@ -11,22 +11,29 @@ float Line(vec2 p, vec2 a, vec2 b, float width, float blur) {
   return smoothstep(width - blur, width, dist);
 }
 
+// Assemble an error pattern use line function
+float errorPattern(vec2 uv, vec2 pos, float length, float thickness, float blur) {
+  float hl = length / 2.;
+  float l1 = Line(uv, vec2(pos-hl), vec2(pos+hl), thickness, blur);
+  float l2= Line(uv, vec2(pos.x-hl, pos.y+hl), vec2(pos.x+hl, pos.y-hl), thickness, blur);
+  return min(l1, l2);
+}
+
 void main() {
   // Normalized pixel coordinates (from 0 to 1)
 	vec2 uv = (2.0 * gl_FragCoord.xy - iResolution.xy) / min(iResolution.y, iResolution.x);
 
-  // Get more smooth edge and setting line width
-  float l1 = Line(uv, vec2(-0.5), vec2(0.5), .1, 0.01);
-  float l2= Line(uv, vec2(-0.5, 0.5), vec2(0.5, -0.5), .1, 0.01);
+  // draw a error icon
+  vec2 ep = vec2(-1., 0.);
+  float m = errorPattern(uv, ep, .5, .04, .007);
 
-  // union all lines
-  float m = min(l1, l2);
-
-  // setting background color
-  vec3 bg = vec3(0.2549, 0.7215, 0.5098);
+  // Time varying pixel color as a background color
+  vec2 buv = gl_FragCoord.xy / iResolution.xy;
+  vec3 bg = 0.5 + 0.5 * cos(iTime + buv.xyx + vec3(0, 2, 4));
 
   // setting frontground color
-  vec3 front = vec3(1);
+  vec2 fuv = fract((uv - ep) / 0.7 + 0.5);
+  vec3 front = 0.5 + 0.5 * cos(iTime + fuv.xyx + vec3(4, 0, 2));
 
   // mix bg-front color
   vec3 col = mix(front, bg, m);
